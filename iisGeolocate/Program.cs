448b061c7a4741164e051031c2046b60fb4900fc
EricZimmerman <saericzimmerman@gmail.com>
2019-06-25 14:28:59 -0400

    project dead. who uses spaces for colum separators and dynamic columns?

--- iisGeolocate/Program.cs
+++ iisGeolocate/Program.cs
@@ -14,15 +14,8 @@ namespace iisGeolocate
 {
     internal class Program
     {
-
         private static Logger _logger;
         private static FluentCommandLineParser<ApplicationArguments> _fluentCommandLineParser;
-
-        internal class ApplicationArguments
-        {
-            public string LogDirectory { get; set; }
-          
-        }
 
         private static void SetupNLog()
         {
@@ -58,6 +51,12 @@ namespace iisGeolocate
                 .WithDescription(
                     "The directory that contains IIS logs. If not specified, defaults to same directory as executable")
                 .SetDefault(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            _fluentCommandLineParser.Setup(arg => arg.FieldName)
+                .As('f')
+                .WithDescription(
+                    "The field name to find to do the geolocation on. Default is 'c-ip'")
+                .SetDefault("c-ip");
 
             _logger = LogManager.GetCurrentClassLogger();
 
@@ -95,13 +94,6 @@ namespace iisGeolocate
                 Directory.CreateDirectory(outDir);
             }
 
-            //#Software: Microsoft Internet Information Services 6.0
-            //#Version: 1.0
-            //#Date: 2016-01-01 00:00:11
-            //#Fields: date time s-sitename s-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) sc-status sc-substatus sc-win32-status 
-            //2016-01-01 00:00:11 W3SVC1 192.168.22.6 GET /iisstart.htm - 443 - 205.210.42.86 easyDNS+Monitoring+(+http://easyurl.net/monitoring+) 200 0 0
-            //2016-01-01 00:02:03 W3SVC1 192.168.22.6 POST /Autodiscover/Autodiscover.xml - 443 WRIGHTUSA\dweber 192.168.22.210 Microsoft+Office/12.0+(Windows+NT+6.1;+Microsoft+Office+Outlook+12.0.6739;+Pro) 200 0 0
-
             var uniqueIps = new Dictionary<string, string>();
 
             var logFiles = Directory.GetFiles(_fluentCommandLineParser.Object.LogDirectory, "*.log");
@@ -116,7 +108,7 @@ namespace iisGeolocate
                 return;
             }
 
-            var cIpSlot = -1;
+            var dataSlot = -1;
 
             if (File.Exists("GeoLite2-City.mmdb") == false && File.Exists("GeoIP2-City.mmdb") == false)
             {
@@ -128,14 +120,12 @@ namespace iisGeolocate
 
             if (File.Exists("GeoIP2-City.mmdb"))
             {
-                _logger.Info($"Found 'GeoIP2-City.mmdb', so using that vs lite...");
+                _logger.Info("Found 'GeoIP2-City.mmdb', so using that vs lite...");
                 dbName = "GeoIP2-City.mmdb";
             }
-            
-            //GeoIP2-City.mmdb
 
             _logger.Warn(
-                $"NOTE: multicast, private, or reserved addresses will be SKIPPED (including IPv6 that starts with 'fe80'");
+                "NOTE: multicast, private, or reserved addresses will be SKIPPED (including IPv6 that starts with 'fe80'");
 
 
             using (var reader = new DatabaseReader(dbName))
@@ -147,8 +137,9 @@ namespace iisGeolocate
                     var baseFilename = Path.GetFileName(file);
                     var outFilename = Path.Combine(outDir, baseFilename);
 
-                    using (var outstream = new StreamWriter(File.OpenWrite(outFilename)))
+                    using (var outstream = new StreamWriter(File.Open(outFilename,FileMode.OpenOrCreate,FileAccess.Write,FileShare.Read)))
                     {
+
                         using (var instream = File.OpenText(file))
                         {
                             var line = instream.ReadLine();
@@ -161,24 +152,28 @@ namespace iisGeolocate
 
                             while (line != null)
                             {
-                                if (line.StartsWith("#Fields"))
-                                {
-                                    line = line.Trim() + " GeoCity GeoCountry";
-                                    var fields = line.Split(' ');
-                                    var pos = 0;
-                                    _logger.Info("Looking for/verifying 'c-ip' field position...");
-                                    foreach (var field in fields)
-                                    {
-                                        if (field.Equals("c-ip"))
-                                        {
-                                            cIpSlot = pos - 1; //account for #Fields: 
-
-                                            _logger.Info($"Found 'c-ip' field position in column '{cIpSlot}'!");
-                                            break;
-                                        }
-                                        pos += 1;
-                                    }
-                                }
+                                //                                if (line.StartsWith("#Fields"))
+//                                {
+//                                    line = line.Trim() + " GeoCity GeoCountry";
+//                                    var fields = line.Split(' ');
+//                                    var pos = 0;
+//                                    _logger.Info(
+//                                        $"Looking for/verifying '{_fluentCommandLineParser.Object.FieldName}' field position...");
+//                                    foreach (var field in fields)
+//                                    {
+//                                        if (field.Equals(_fluentCommandLineParser.Object.FieldName,
+//                                            StringComparison.OrdinalIgnoreCase))
+//                                        {
+//                                            dataSlot = pos - 1; //account for #Fields: 
+//
+//                                            _logger.Info(
+//                                                $"Found '{_fluentCommandLineParser.Object.FieldName}' field position in column '{dataSlot}'!");
+//                                            break;
+//                                        }
+//
+//                                        pos += 1;
+//                                    }
+//                                }
 
                                 var geoCity = "NA";
                                 var geoCountry = "NA";
@@ -186,45 +181,51 @@ namespace iisGeolocate
                                 if (line.StartsWith("#") == false)
                                 {
                                     var segs = line.Split(' ');
-                                    var ip = segs[cIpSlot];
+
+                                    dataSlot = segs.Length - 1;
+
+                                    var ip = segs[dataSlot].Replace("\"","");
 
                                     if (ip.StartsWith("fe80"))
-
-                                        try
                                     {
-                                        var segs2 = ip.Split('.');
-                                        if (segs2.Length> 1)
-                                        {
-                                            var first = int.Parse(segs2[0]);
-                                            var second = int.Parse(segs2[1]);
+                                        continue;
+                                    }
 
-                                            if (first >= 224 || first == 10 || first == 192 && second == 168 || 
-                                                first == 172 && second >= 16 && second <= 31)
+                                    {
+                                        try
+                                        {
+                                            var segs2 = ip.Split('.');
+                                            if (segs2.Length > 1)
                                             {
-                                            
-                                                continue;
+                                                var first = int.Parse(segs2[0]);
+                                                var second = int.Parse(segs2[1]);
+
+                                                if (first >= 224 || first == 10 || first == 192 && second == 168 ||
+                                                    first == 172 && second >= 16 && second <= 31)
+                                                {
+                                                    continue;
+                                                }
+                                            }
+
+                                            var city = reader.City(ip);
+                                            geoCity = city.City?.Name?.Replace(' ', '_');
+
+                                            geoCountry = city.Country.Name.Replace(' ', '_');
+
+                                            if (uniqueIps.ContainsKey(ip) == false)
+                                            {
+                                                uniqueIps.Add(ip, $"{geoCity}, {geoCountry}");
                                             }
                                         }
-
-                                        var city = reader.City(ip);
-                                        geoCity = city.City?.Name?.Replace(' ', '_');
-
-                                        geoCountry = city.Country.Name.Replace(' ', '_');
-
-                                        if (uniqueIps.ContainsKey(ip) == false)
+                                        catch (AddressNotFoundException an)
                                         {
-                                            uniqueIps.Add(ip, $"{geoCity}, {geoCountry}");
                                         }
-                                    }
-                                    catch (AddressNotFoundException an)
-                                    {
-                                        
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        _logger.Info($"Error: {ex.Message} for line: {line}");
-                                        geoCity = $"City error: {ex.Message}";
-                                        geoCountry = "Country error: (See city error)";
+                                        catch (Exception ex)
+                                        {
+                                            _logger.Info($"Error: {ex.Message} for line: {line}");
+                                            geoCity = $"City error: {ex.Message}";
+                                            geoCountry = "Country error: (See city error)";
+                                        }
                                     }
 
                                     line += $" {geoCity} {geoCountry}";
@@ -261,6 +262,12 @@ namespace iisGeolocate
 
                 uniqOut.Flush();
             }
+        }
+
+        internal class ApplicationArguments
+        {
+            public string LogDirectory { get; set; }
+            public string FieldName { get; set; }
         }
     }
 }
