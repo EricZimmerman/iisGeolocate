e828ed8dee2ad2c9df96f8f1d667a724f64c84bd
Eric Zimmerman <saericzimmerman@gmail.com>
2021-12-13 21:39:38 -0500

    modernize sln

--- iisGeolocate/Properties/AssemblyInfo.cs
+++ iisGeolocate/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("iisGeolocate")]
-[assembly: AssemblyDescription("Add geolocation information to IIS logs")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eric R. Zimmerman 501-313-3778")]
-[assembly: AssemblyProduct("iisGeolocate")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fcdf899c-d8a8-43c4-8540-b4cd9e7fe791")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.2")]
-[assembly: AssemblyFileVersion("2.0.0.2")]
